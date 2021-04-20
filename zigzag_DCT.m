38c545ab15f1d10365c33c61541b5c3f7fcf04be
PrakharKSingh <82879279+PrakharKSingh@users.noreply.github.com>
2021-04-20 17:39:17 +0530

    Create zigzag_DCT.m

---
+++ zigzag_DCT.m
@@ -0,0 +1,52 @@
+clc; clear all; close all; 
+image = rgb2gray(imread('Lenna.jpg')); 
+vmax = size(image, 1); 
+hmax = size(image, 2); 
+vmin = 1; 
+hmin = 1; 
+v = vmin; 
+h = hmin; 
+output = zeros(1, vmax*hmax); 
+%% Algorithm for zig zag pattern 
+i = 1; 
+while ((v <= vmax) && (h <= hmax)) 
+if (mod(h + v, 2) == 0) % going up 
+if (v == vmin) 
+output(i) = image(v, h); % if we got to the first line if (h == hmax) 
+v = v + 1; 
+else 
+h = h + 1; 
+end 
+i = i + 1; 
+elseif ((h == hmax) && (v < vmax)) % if we got to the last column output(i) = image(v, h); 
+v = v + 1; 
+i = i + 1; 
+elseif ((v > vmin) && (h < hmax)) % all other cases 
+output(i) = image(v, h); 
+v = v - 1; 
+h = h + 1; 
+i = i + 1; 
+end 
+else % going down 
+if ((v == vmax) && (h <= hmax)) % if we got tothe last line output(i) = image(v, h); 
+h = h + 1; 
+i = i + 1; 
+elseif (h == hmin) % if we got to the first column output(i) = image(v, h); 
+if (v == vmax) 
+h = h + 1; 
+else 
+v = v + 1; 
+end 
+i = i + 1; 
+elseif ((v < vmax) && (h > hmin)) 
+output(i) = image(v, h); 
+v = v + 1; 
+h = h - 1; 
+i = i + 1; 
+end 
+end 
+if ((v == vmax) && (h == hmax)) % bottom right element 
+output(i) = image(v, h); 
+break 
+end 
+end
